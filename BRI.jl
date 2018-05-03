a760d28e5254d895ae6b9f88a529f62c4ddedeef
Cohen <cohenbp@nih.gov>
2018-05-03 18:36:30 -0400

    Trying symmetric inputs followed by asymmetric, or vice versea

---
+++ BRI.jl
@@ -0,0 +1,324 @@
+function emptiness(x, funk, error_code) #checks if an empty set got populated
+  if length(x) > 0
+    return funk(x)
+  else
+    return error_code
+  end
+end
+
+function cv(isi)
+  SD = std(isi)
+  return SD/mean(isi)
+end
+
+function Brent_W(N, k, Jee, Jie, Jei, Jii)
+
+  half = round(Int64, N/2)
+  sk = sqrt(k)
+  W = zeros(N, N)
+
+  Jee /= sk
+  Jie /= sk
+  Jei /= sk
+  Jii /= sk
+
+  #Excitatory Neurons
+  for i = 1:half
+    ee_inds = rand(1:half, k)
+    ei_inds = rand(half+1:N, k)
+    for j in eachindex(ee_inds)
+      W[i, ee_inds[j]] += Jee
+      W[i, ei_inds[j]] += Jei
+    end
+  end
+
+  #Inhibitory Neurons
+  for i = half+1:N
+    ie_inds = rand(1:half, k)
+    ii_inds = rand(half+1:N, k)
+    for j in eachindex(ie_inds)
+      W[i, ie_inds[j]] += Jie
+      W[i, ii_inds[j]] += Jii
+    end
+  end
+
+  return W
+end
+
+#indices for parse column representation
+function sparse_rep(W,N)
+    flat = []
+    for i = 1:N
+        mi = find(W[:,i])
+        push!(flat, mi)
+    end
+    return flat
+end
+
+
+  #20000 thousand neurons, 10000 E and 10000 I
+  #each presynaptic neuron connects to 2500 random E and 2500 random I neurons
+  #sampling includes replacement; can make multiple connections
+  #ee = 12.5, ie = 20., ii=ei = -50, scaled by sqrt(N)
+  #cov(s(t), s(t+tau)) = exp(-tau^2/2(tau^2))
+  #F(t) = sqrt(N)*bias + sigma*s(t)
+
+function OU_Model(R, tau, h)
+  smooth = zeros(length(R))
+  x = rand()
+  scale = sqrt(h)/tau
+  leak = -h/tau
+  R *= scale
+  for i = 1:length(R)
+    x += leak*x + R[i]
+    smooth[i] = x
+  end
+  return smooth
+end
+
+function interpolate_spike(v2, v1, vth)
+  x = (v1-v2) #slope by linear interpolation (dv/dt) = change in voltage for a single time step
+  t = (vth - v2)/x #time since spike to now
+  return t
+end
+
+function Brent_Network_Euler_CSR_A(h, total, CSR, W, N, s1, s2, vth, tau_m, tau_s, tau_a, g_a)
+
+  ntotal = round(Int,total/h)
+  half = round(Int64, N/2)
+  quar = round(Int64, N/4)
+
+  half_ = half + 1
+  quar_ = quar + 1
+
+  p1 = [1:quar] #pool 1 E
+  p2 = [quar_:half] #pool 2 E
+  p3 = [half_:half+quar] #pool 1 I
+  p4 = [half_+quar:N] #pool 2 I
+  s3 = s1 .- .1 #inhibitory neurons get slightly weaker input
+  s4 = s2 .- .1
+
+  V = rand(N)*vth
+  V_buff = V
+
+  syn = zeros(N)
+  A = zeros(N)
+  g_h = g_a * h
+
+  #raster
+  time = Float64[0]
+  raster = Float64[0]
+
+  #Calculate drive and leak constants ahead of time
+  m_leak = h/tau_m
+  s_leak = h/tau_s
+  a_leak = h/tau_a
+
+  for iter = 1:ntotal
+
+    V .+= (h*syn) .- (V*m_leak) .- (g_h*A)
+    V[p1] += s1[iter]
+    V[p2] += s2[iter]
+    V[p3] += s3[iter]
+    V[p4] += s4[iter]
+
+    syn .-= (syn*s_leak)
+    A .-= (A*a_leak)
+
+    #check for spikes
+    vs = (V.>vth)
+    vsm = sum(vs)
+
+    #update excitatory synapses and adaptation
+    if vsm > 0
+      spe = find(vs)
+      for j = 1:vsm
+        js = spe[j]
+        #interpolate spike time
+        delta_h = interpolate_spike(V[js], V_buff[js], vth) #time since the spike (units of h)
+        lx = exp(-delta_h*h/tau_s)
+        syn[CSR[js]] += W[CSR[js], js].*lx #modify amplitude of synapse by decay since estimated time of spike
+        push!(raster,js)
+  	    push!(time,iter-delta_h)
+        A[js] += 1.
+      end
+    end
+
+    #reset after spike
+    V .-= vth*vs
+    V_buff = V
+  end
+
+  return time, raster
+end
+
+function nt_diff(t, r, ntotal, half, netd_binsize)
+
+  netd_bins = collect(1:netd_binsize:ntotal)
+  ntd = zeros(length(netd_bins)-1)
+  nts = zeros(length(netd_bins)-1)
+
+  for j = 2:length(netd_bins)
+    tf = find(netd_bins[j-1] .<= t .< netd_bins[j])
+    T = sum(r[tf] .> half)
+    B = sum(r[tf] .<= half)
+    NTD = T - B #################Differences in Spikes
+    ntd[j-1] = NTD
+    nts[j-1] = length(tf)
+    #println(T+B==length(tf))
+  end
+
+  return ntd, nts
+end
+
+function shmitt_single(s)
+
+  #Handle WTA and Normalization Exceptions
+  if maximum(s) < .5
+    return return ["lose", "end"], [1, length(s)]
+  elseif minimum(s) >= .5
+    return ["win", "end"], [1, length(s)]
+  elseif (.3 <= maximum(s) <= .7) & (.3 <= minimum(s) <= .7)
+    return ["draw", "end"], [1, length(s)]
+  end
+
+  times = []
+  flags = []
+
+  w = findfirst(s .> .5)
+  l = findfirst(s .< .5)
+  f = minimum([w, l])
+  if f == w
+    flag = true
+  else
+    flag = false
+  end
+
+  push!(times, f-1)
+  push!(flags, flag)
+
+  for i = f:length(s)
+    f1 = (s[i] > .5)
+    if f1 != flag
+      push!(times, (i+i-1)/2.)
+      push!(flags, flag)
+      flag = !flag
+    end
+  end
+
+  return flags, times
+end
+
+function WLD_01(s, tl, th)
+  if maximum(s) < tl
+    return ["lose", "end"], [1, length(s)]
+  elseif minimum(s) >= th
+    return ["win", "end"], [1, length(s)]
+  elseif (tl <= maximum(s) <= th) & (tl <= minimum(s) <= th)
+    return ["draw", "end"], [1, length(s)]
+  end
+  times = []
+  flags = []
+  if s[1] >= th
+    flag = "win"
+  elseif tl <= s[1] <= th
+    flag = "draw"
+  elseif s[1] < tl
+    flag = "lose"
+  end
+
+  push!(times, 1)
+  push!(flags, flag)
+
+  s2 = s[2:end]
+  for i in eachindex(s2)
+    f1 = comp_01(s2[i], tl, th)
+    if f1 != flag
+      flag = f1
+      push!(times, i+1)
+      push!(flags, flag)
+    end
+  end
+  push!(times, length(s))
+  push!(flags, "end")
+  return flags, times
+end
+
+function comp_01(x, tl, th)
+  if x > th
+    return "win"
+  elseif tl <= x <= th
+    return "draw"
+  elseif x < tl
+    return "lose"
+  else
+    return "weird"
+  end
+end
+
+function splice_reversions(flags, times)
+  w0 = findfirst(flags, "win")
+  l0 = findfirst(flags, "lose")
+  #assume rivalry, and that any reversions are short and fail to persist
+  empezar = min(w0, l0)
+  nf = [flags[empezar]]
+  a = ["win", "lose"]
+  t = [empezar]
+  if start == w0
+    f = 2
+  else
+    f = 1
+  end
+  flag = a[f]
+  for i = empezar:length(flags)
+    if flags[i] == a[f]
+      push!(t, times[i])
+      push!(nf, flags[i])
+      a = circshift(a, 1)
+    end
+  end
+  push!(t, times[end])
+  return t, nf
+end
+
+function splice_flags(flags, times)
+  l = find(flags.=="lose")
+  w = find(flags.=="win")
+  d = find(flags.=="draw")
+  bot = [[((times[i]-1)*250)+1, ((times[i+1]-1)*250)] for i in l]
+  top = [[((times[i]-1)*250)+1, ((times[i+1]-1)*250)] for i in w]
+  nmz = [[((times[i]-1)*250)+1, ((times[i+1]-1)*250)] for i in d]
+  # bot = [[times[i], times[i+1]] for i in l]*250
+  # top = [[times[i], times[i+1]] for i in w]*250
+  # nmz = [[times[i], times[i+1]] for i in d]*250
+  bdom = emptiness([(i[2]-i[1]) for i in bot], sum, 0)
+  tdom = emptiness([(i[2]-i[1]) for i in top], sum, 0)
+  tnmz = emptiness([(i[2]-i[1]) for i in nmz], sum, 0)
+  # bdom = emptiness([250*(i[2]-i[1]) for i in bot], sum, 0)
+  # tdom = emptiness([250*(i[2]-i[1]) for i in top], sum, 0)
+  # tnmz = emptiness([250*(i[2]-i[1]) for i in nmz], sum, 0)
+  return top, tdom, bot, bdom, nmz, tnmz
+end
+
+#downsample A so it is the same size as B
+function downsample(A, B)
+  LB = length(B)
+  f = Int64(floor(length(A)/LB))
+  L = Int64(f*LB)
+  A_ = A[1:Int64(L)] #Truncated version of A so B fits into it an even number of times
+  println(length(A_))
+  A_S = zeros(LB) #subsample of A
+  for i = 1:LB-1
+    println(i)
+    A_S[i] = A_[((i-1)*f)+1]
+  end
+  return A_S
+end
+
+function zscore(x)
+    m = mean(x)
+    s = std(x)
+    x1 = x .- m
+    x2 = x1 ./s
+    return x2
+end
--- run_brent_riv.jl
+++ run_brent_riv.jl
@@ -1,5 +1,5 @@
-#
-include("brent_riv.jl")
+#include("brent_riv.jl")
+include("BRI.jl")
 include("c://Users\\cohenbp\\Documents\\Neuroscience\\Balance_Circuits\\two_pools\\Analyze.jl")
 
 using Distributions
@@ -14,12 +14,13 @@ srand(1234)
 # wta = []
 # for i in [2000, 4000, 8000, 16000, 32000]
 
-N = 20000
+N = 5000
 half = div(N,2)
 quar = div(half,2)
 runtime = 10000
 rt = runtime /1000.
 h = 0.1
+ntotal = Int64(runtime/h)
 
 Jee = 12.5
 Jie = 20.
@@ -29,9 +30,8 @@ Jii = -50.
 vth = 20.
 tau_m = 20.
 tau_s = 2.
-tau_a = 5000.
-g_a = 3000.0/tau_a
-g_a = 0.
+tau_a = 1000.
+g_a = 5000.0/tau_a
 
 p = 1/8.
 k = 2500
@@ -39,64 +39,110 @@ k = 2500
 BW = Brent_W(N, k, Jee, Jie, Jei, Jii)
 CSR = sparse_rep(BW, N)
 
-s1 = 2.7
-s2 = 2.4
+s1 = zeros(ntotal) .+ 2.4*h
+s2 = zeros(ntotal) .+ 2.*h
 
-t, r = Brent_Network_Euler_CSR_A(h, runtime, CSR, BW, N, s1, s1, vth, tau_m, tau_s, tau_a, g_a)
+# s1[30001:ntotal] = 2.7*h
 
-e_m = find(r .<= half);
-i_m = find(r .> half);
-ER = length(e_m)*(1/rt)*(1/half);
-IR = length(i_m)*(1/rt)*(1/half);
-te_pt = t[e_m];
-re_pt = r[e_m];
-ti_pt = t[i_m];
-ri_pt = r[i_m];
 
-wta_ness, bias = score_analysis(re_pt, half)
-push!(wta, wta_ness)
+@time t, r = Brent_Network_Euler_CSR_A(h, runtime, CSR, BW, N, s1, s2, vth, tau_m, tau_s, tau_a, g_a)
 
-# eight = div(quar, 2)
-# plot(t/10000., r, "g.", ms = 1.)
+subplot(211)
+title("Brent Network in Response to Various Inputs")
+ylabel("Neuron #")
+plot(t ./ 10000, r, ".", ms = 1.)
+subplot(212)
+plot(linspace(0,rt,ntotal), s1, alpha = .5)
+plot(linspace(0,rt,ntotal), s2, alpha = .5)
+plot(0, 0, "r.", alpha = 0.)
+xlabel("Time")
+ylabel("Input (mV/ms)")
+
+# e_m = find(r .<= half);
+# i_m = find(r .> half);
+# ER = length(e_m)*(1/rt)*(1/half);
+# IR = length(i_m)*(1/rt)*(1/half);
+# te_pt = t[e_m];
+# re_pt = r[e_m];
+# ti_pt = t[i_m];
+# ri_pt = r[i_m];
 #
-# pools = ["E1", "E2", "I1", "I2"]
-# y = [eight, eight + quar, eight + half, eight + half + quar]
-# for i = 1:length(y)
-#              annotate(pools[i], xy = (-.4, y[i]))
-# end
-# ylabel("Neuron Index")
-# xlabel("Time (s)")
+# e1 = find(te_pt .> quar)
+# e2 = find(te_pt .<= quar)
+# te1 = te_pt[e1]
+# re1 = re_pt[e1]
+# te2 = te_pt[e2]
+# re2 = re_pt[e2]
 #
-# title("Random Network w/ Asymmetric Inputs\ng_a=$(g_a)")
-# function s2ta(A, k, tau_s)
-#     return A*sqrt(k)*tau_s/1000.
-# end
-
-Wee=s2ta(abs(Jee), k, tau_s)
-Wei=s2ta(abs(Jei), k, tau_s)
-Wie=s2ta(abs(Jie), k, tau_s)
-Wii=s2ta(abs(Jii), k, tau_s)
-
-RE1_2x2, RI1_2x2 = theory_rates_2x2(Wee, Wie, Wei, Wii, max(s1, s2), max(s1, s2)-.1)
-E_R_top = [length(find(re_pt .== i))/rt for i=quar+1:half]
-E_R_bot = [length(find(re_pt .== i))/rt for i=1:quar]
-I_R_top = [length(find(ri_pt .== i))/rt for i=half+1:half+quar]
-I_R_bot = [length(find(ri_pt .== i))/rt for i=half+quar+1:N]
-
-MER1 = mean(E_R_bot)#*(1/1000.)
-MER2 = mean(E_R_top)#*(1/1000.)
-MIR1 = mean(I_R_bot)#*(1/1000.)
-MIR2 = mean(I_R_top)#*(1/1000.)
-
-# push!(ER1, MER1)
-# push!(ER2, MER2)
-# push!(IR1, MIR1)
-# push!(IR2, MIR2)
-# push!(TRE, RE1_2x2)
-# push!(TRI, RI1_2x2)
-
+# netd_bins = collect(1:netd_binsize:ntotal)
+# nta = zeros(length(netd_bins)-1)
+# ntb = zeros(length(netd_bins)-1)
+#
+# for j = 2:length(netd_bins)
+#   tf = find(netd_bins[j-1] .<= te_pt .< netd_bins[j])
+#   T = sum(re_pt[tf] .> quar)
+#   B = sum(re_pt[tf] .<= quar)
+#   nta[j-1] = T
+#   ntb[j-1] = B
 # end
 #
+# ntas  = 10000. .* (nta ./ quar) ./netd_binsize
+# ntbs  = 10000. .* (ntb ./ quar) ./netd_binsize
+
+
+# subplot(211)
+# title("Strong Adaptation")
+# plot(t, r, ".", ms = 1.)
+# subplot(212)
+# ylabel("Mean Firing Rate")
+# plot(ntas)
+# plot(ntbs)
+
+#
+# wta_ness, bias = score_analysis(re_pt, half)
+# push!(wta, wta_ness)
+#
+# # eight = div(quar, 2)
+# # plot(t/10000., r, "g.", ms = 1.)
+# #
+# # pools = ["E1", "E2", "I1", "I2"]
+# # y = [eight, eight + quar, eight + half, eight + half + quar]
+# # for i = 1:length(y)
+# #              annotate(pools[i], xy = (-.4, y[i]))
+# # end
+# # ylabel("Neuron Index")
+# # xlabel("Time (s)")
+# #
+# # title("Random Network w/ Asymmetric Inputs\ng_a=$(g_a)")
+# # function s2ta(A, k, tau_s)
+# #     return A*sqrt(k)*tau_s/1000.
+# # end
+#
+# Wee=s2ta(abs(Jee), k, tau_s)
+# Wei=s2ta(abs(Jei), k, tau_s)
+# Wie=s2ta(abs(Jie), k, tau_s)
+# Wii=s2ta(abs(Jii), k, tau_s)
+#
+# RE1_2x2, RI1_2x2 = theory_rates_2x2(Wee, Wie, Wei, Wii, max(s1, s2), max(s1, s2)-.1)
+# E_R_top = [length(find(re_pt .== i))/rt for i=quar+1:half]
+# E_R_bot = [length(find(re_pt .== i))/rt for i=1:quar]
+# I_R_top = [length(find(ri_pt .== i))/rt for i=half+1:half+quar]
+# I_R_bot = [length(find(ri_pt .== i))/rt for i=half+quar+1:N]
+# #
+# MER1 = mean(E_R_bot)#*(1/1000.)
+# MER2 = mean(E_R_top)#*(1/1000.)
+# MIR1 = mean(I_R_bot)#*(1/1000.)
+# MIR2 = mean(I_R_top)#*(1/1000.)
+#
+# # push!(ER1, MER1)
+# # push!(ER2, MER2)
+# # push!(IR1, MIR1)
+# # push!(IR2, MIR2)
+# # push!(TRE, RE1_2x2)
+# # push!(TRI, RI1_2x2)
+#
+# # end
+# #
 # plot([2000, 4000, 8000, 16000, 32000], ER1, ".", ms = 10., label = "Sim: ER1")
 # plot([2000, 4000, 8000, 16000, 32000], ER2, ".", ms = 10., label = "Sim: ER2")
 # plot([2000, 4000, 8000, 16000, 32000], IR1, ".", ms = 10., label = "Sim: IR1")
